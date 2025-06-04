efa76bdd9446bed6171825f500ebafd2781b069a
Ириска <135683471+iriska1488@users.noreply.github.com>
2025-06-04 18:02:01 +0300

    Update IriskAutoGamerule.java

--- src/main/java/wtf/iriska/IriskAutoGamerule.java
+++ src/main/java/wtf/iriska/IriskAutoGamerule.java
@@ -16,7 +16,7 @@ public class IriskAutoGamerule extends JavaPlugin {
         spawnWorldName = getConfig().getString("spawn-world", "spawn");
 
         getLogger().info("IriskAutoGamerule запущен! Проверяем геймрулы...");
-
+        
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, this::checkAndDisableGamerules, 20L);
     }
 
@@ -30,25 +30,30 @@ public class IriskAutoGamerule extends JavaPlugin {
 
             // Специальные настройки для мира спавна
             if (world.getName().equalsIgnoreCase(spawnWorldName)) {
-                // Отключаем доступные виды урона в 1.16.5
+                // Отключаем урон
                 checkAndDisableGamerule(world, GameRule.DROWNING_DAMAGE, false);
                 checkAndDisableGamerule(world, GameRule.FALL_DAMAGE, false);
                 checkAndDisableGamerule(world, GameRule.FIRE_DAMAGE, false);
 
-                // Отключаем доступные варианты спавна мобов в 1.16.5
+                // Отключаем спавн мобов
                 checkAndDisableGamerule(world, GameRule.DO_MOB_SPAWNING, false);
                 checkAndDisableGamerule(world, GameRule.DO_PATROL_SPAWNING, false);
                 checkAndDisableGamerule(world, GameRule.DO_TRADER_SPAWNING, false);
+
+                // Отключаем физику и циклы мира
+                checkAndDisableGamerule(world, GameRule.RANDOM_TICK_SPEED, 0);
+                checkAndDisableGamerule(world, GameRule.DO_DAYLIGHT_CYCLE, false);
+                checkAndDisableGamerule(world, GameRule.DO_WEATHER_CYCLE, false);
             }
         }
-
+        
         getLogger().info("Проверка геймрулов завершена!");
     }
 
     private <T> void checkAndDisableGamerule(World world, GameRule<T> gamerule, T desiredValue) {
         try {
             T currentValue = world.getGameRuleValue(gamerule);
-
+            
             if (currentValue != null && !currentValue.equals(desiredValue)) {
                 world.setGameRule(gamerule, desiredValue);
                 getLogger().info(String.format("Изменен геймрул %s в мире %s с %s на %s",
@@ -58,8 +63,8 @@ public class IriskAutoGamerule extends JavaPlugin {
                         desiredValue));
             }
         } catch (Exception e) {
-            getLogger().warning("Не удалось изменить геймрул " + gamerule.getName() +
-                    " для мира " + world.getName() + ": " + e.getMessage());
+            getLogger().warning("Не удалось изменить геймрул " + gamerule.getName() + 
+                              " для мира " + world.getName() + ": " + e.getMessage());
         }
     }
 
